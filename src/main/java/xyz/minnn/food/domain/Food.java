d15ea07fecfc7c75fa72ea3c86a27192e5ed8068
minnnkyeng <minnn.xyz@gmail.com>
2023-04-22 00:12:17 +0900

    feat : Food 구현

---
+++ src/main/java/xyz/minnn/food/domain/Food.java
@@ -0,0 +1,12 @@
+package xyz.minnn.food.domain;
+
+public class Food {
+
+    private String name;
+    private int price;
+    private String category;
+    private String size;
+    private int quantity;
+    private String icon;
+
+}
